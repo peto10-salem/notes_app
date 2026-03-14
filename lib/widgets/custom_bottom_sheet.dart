545e356038a973ffb7bf310f8c2a463a262e1d3e
peter <petertemo885@gmail.com>
2026-03-14 12:58:17 +0200

    create add notes cubit and add note states

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -11,23 +11,61 @@ class AddNoteNottomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32),
-            const CustomTextField(
-              hintText: "Title",
-            ),
-            SizedBox(height: 16),
-            CustomTextField(
-              hintText: "content",
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            customButton(),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey();
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hintText: "Title",
+          ),
+          SizedBox(height: 16),
+          CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
+            hintText: "content",
+            maxLines: 5,
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          customButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,27 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class customButton extends StatelessWidget {
-  const customButton({super.key});
-
+  const customButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: kPrimaryColour,
-      ),
-      child: Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: kPrimaryColour,
+        ),
+        child: Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
         ),
+        height: 60,
+        width: MediaQuery.of(context).size.width,
       ),
-      height: 60,
-      width: MediaQuery.of(context).size.width,
     );
   }
 }
